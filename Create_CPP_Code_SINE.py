1ee22d689dad724508abdd6eca44fd832033a575
Wesley McGinn <wesleymcginn1@gmail.com>
2021-10-29 16:03:01 -0400

    Delete Create_CPP_Code_SINE.py
    An extra array of trigonometric values was not needed because, rather than doing the same thing that was done with cosine in FINAL.cpp, this can be done:
    
    double sin(double m) {
      return cos(m-90);
    }
    
    This is because of the math fact:
    sinθ = cos(θ-90)
    (When θ is in degrees)

--- Create_CPP_Code_SINE.py
+++
@@ -1,9 +0,0 @@
-# Program made by Wesley McGinn
-# Writes C++ Code in the Python Shell that can be copied and pasted into a sketch
-
-import math
-pi = 3.141592653589793
-Loop = 0
-while Loop <= 360:
-  print("if (n == " + str(Loop) + ") { return " + str(math.sin((pi/180)*Loop)) + "; }")
-  Loop += 1
